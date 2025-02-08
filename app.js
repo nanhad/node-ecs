ad89d4f9654c799b230c339a6b9fd084aea8021c
nanhad <faisalganjadeveloper@gmail.com>
2025-02-08 08:30:59 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
 
-const path = __dirname + '/views/';
+// Define a health check endpoint
+app.use('/health', (req, res) => {
+  res.status(200).send('OK'); // Respond with a status code of 200 and 'OK'
+});
+
+// Start the server on a specific port (e.g., 8080)
 const port = 8080;
-
-router.use(function (req,res,next) {
-  console.log('/' + req.method);
-  next();
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
-router.get('/', function(req,res){
-  res.sendFile(path + 'index.html');
-});
-
-router.get('/sharks', function(req,res){
-  res.sendFile(path + 'sharks.html');
-});
-
-app.use(express.static(path));
-app.use('/', router);
-
-app.listen(port, function () {
-  console.log('Example app listening on port 8080!')
-})
